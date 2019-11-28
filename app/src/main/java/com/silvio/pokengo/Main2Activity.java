bff3424bfffaecac77667644895d8e1b1a4ec74b
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-28 14:49:19 -0300

    segundo commit kkkk

--- app/src/main/java/com/silvio/pokengo/Main2Activity.java
+++ app/src/main/java/com/silvio/pokengo/Main2Activity.java
@@ -3,12 +3,36 @@ package com.silvio.pokengo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private ImageButton imageButton;
+    private ImageView imageView;
+    private TextView textView;
+
+    private int[] images = {R.drawable.fogo, R.drawable.planta};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        imageButton = findViewById(R.id.button);
+        imageView = findViewById(R.id.imagem);
+        textView = findViewById(R.id.jogador);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(images[new Random().nextInt(images.length)]);
+            }
+        });
+
     }
 }
