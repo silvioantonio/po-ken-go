e342897498d386224463338e59bfa8468025b865
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-30 09:28:52 -0300

    Adicionado musica tema a tela inicial do jogo

--- app/src/main/java/com/silvio/pokengo/MainActivity.java
+++ app/src/main/java/com/silvio/pokengo/MainActivity.java
@@ -3,6 +3,7 @@ package com.silvio.pokengo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button novo_jogo, resetar_jogo;
     private Intent intent;
+    private MediaPlayer mediaPlayer;
     private String score;
 
     @Override
@@ -21,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         novo_jogo = findViewById(R.id.novo_jogo);
         resetar_jogo = findViewById(R.id.resetar_jogo);
 
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.opening);
+
+        mediaPlayer.start();
+
         /*if(getIntent().getExtras().getString("score") != null) {
             score = getIntent().getExtras().getString("score");
         } else {
@@ -30,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         novo_jogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                }
                 intent = new Intent(MainActivity.this, Main2Activity.class);
                 intent.putExtra("score", score);
                 startActivity(intent);
